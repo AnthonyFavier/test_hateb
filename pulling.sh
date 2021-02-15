f3e00d24316ca4a4aa8071feab41f5e2de73af03
Anthony <anthony.favier@laas.fr>
2021-02-15 14:49:09 +0100

    pulling better

--- pulling.sh
+++ pulling.sh
@@ -1,8 +1,8 @@
 cd src/
-git clone -b master https://github.com/AnthonyFavier/hanp_prediction
-git clone -b melodic-devel-holonomous https://github.com/AnthonyFavier/laas_navigation
-git clone -b melodic-devel https://github.com/AnthonyFavier/morse_ros
-git clone -b melodic_devl https://github.com/sphanit/hanp_msgs
-git clone -b hateb_new https://github.com/sphanit/hateb_local_planner
-git clone -b melodic-devel https://github.com/sphanit/pr2_common_actions
+git clone -b master 			https://github.com/AnthonyFavier/hanp_prediction
+git clone -b melodic-devel-holonomous 	https://github.com/AnthonyFavier/laas_navigation
+git clone -b melodic-devel 		https://github.com/AnthonyFavier/morse_ros
+git clone -b melodic_devl 		https://github.com/sphanit/hanp_msgs
+git clone -b hateb_new 			https://github.com/sphanit/hateb_local_planner
+git clone -b melodic-devel 		https://github.com/sphanit/pr2_common_actions
 cd ..
